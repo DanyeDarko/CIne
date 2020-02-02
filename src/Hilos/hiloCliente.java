89dc11cdd6661a6eeadf89ac78c66c3955736d1f
Danye <danielbrzen@outlook.com>
2020-02-01 18:42:55 -0600

    HILO ENCARGADO DE INSERTAR COMPRA O DEVOLUCIONES EN EL ARREGLO DE TAQUILLAS

---
+++ src/Hilos/hiloCliente.java
@@ -0,0 +1,23 @@
+package Hilos;
+
+import Objetos.Cine;
+
+public class hiloCliente extends Thread {
+
+    int cliente, numeroTaquilla, cantBoletos, numSala, tipoPeracion;
+    Cine Cine;
+    public hiloCliente(Cine c,int cliente, int numeroTaquilla, int cantBoletos, int numSala, int tipoOperacion) {
+        this.Cine = c;
+        this.cliente = cliente;
+        this.numeroTaquilla = numeroTaquilla;
+        this.cantBoletos = cantBoletos;
+        this.numSala = numSala;
+        this.tipoPeracion = tipoOperacion;
+
+    }
+
+    public void run() {
+    Cine.operacionTaquilla(cliente, numeroTaquilla, cantBoletos,numSala,tipoPeracion);
+    }
+
+}
