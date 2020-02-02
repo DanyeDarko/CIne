ecca58b8d74893b0f6bf792c1b800796bc1d7733
Danye <danielbrzen@outlook.com>
2020-02-01 18:45:10 -0600

    INTERFAZ CON METODOS ABSTRACTOS PARA LA IMPLEMENTACION SOBRE LA CLASE 'DatosCineAbstracta' TAREAS REGULARES DE CINE

---
+++ src/Interfaces/InterfazCine.java
@@ -0,0 +1,10 @@
+package Interfaces;
+
+
+public interface InterfazCine {
+
+    public void obtenerDatosTaquilla(int numeroTaquilla);
+
+    public void operacionTaquilla(int numeroCliente, int numTaquilla, int cantidadBoletos, int numeroSala, int tipoOperacion);
+
+}
