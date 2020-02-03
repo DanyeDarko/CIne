406cbb9fb4bd4dd804a23a9822de98bf4095d9a0
Danye <danielbrzen@outlook.com>
2020-02-02 22:08:42 -0600

    Nuevo metodo para leer matriz de archivo plano

--- src/Interfaces/InterfazCine.java
+++ src/Interfaces/InterfazCine.java
@@ -7,4 +7,8 @@ public interface InterfazCine {
     public void operacionTaquilla(int numeroCliente, int numTaquilla, int cantidadBoletos, int numeroSala, int tipoOperacion);
 
     public void LlenarTaquillas(int numeroTaquillaAllenar);
+
+    public Integer[][] leerMatrizTxt(String URLArchivo);
+
+    // public void contabilisarVentasTotales();
 }
